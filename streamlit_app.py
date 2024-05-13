464aff17350e618c0fc9d8a4833afd9f3d782872
davis011235 <157519728+davis011235@users.noreply.github.com>
2024-05-13 14:14:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
-#Importing packages
+# Importing packages
 import streamlit as st
 import pandas as pd
 
 data_url = "https://raw.githubusercontent.com/davis011235/ECON8320-Project/main/df.csv"
 
+# Caching data so the dashboard runs faster
 @st.cache_data
 def load_data(url):
     df = pd.read_csv(url)
@@ -11,23 +12,24 @@ def load_data(url):
 
 data = load_data(data_url)
 
+# Adding title
 st.title("ECON 8320 Project")
 
+# Creating selectbox for x axis variable
 x = st.sidebar.selectbox(
     'x variable',
     ('Year', 'Hours Usually Worked', 'Household Income',
        'Household Members', 'HS or GED', 'Years of College',
        'Ever in Military', 'Currently in Military', 'Union Member', 'City')
 )
+# Creating selectbox for y axis variable
 y = st.sidebar.selectbox(
     'y variable',
     ('Year', 'Hours Usually Worked', 'Household Income',
        'Household Members', 'HS or GED', 'Years of College',
        'Ever in Military', 'Currently in Military', 'Union Member', 'City')
 )
-
-st.sidebar.checkbox(
-    'Filter by City', False)
-
+    
+# Generating scatter plot of x and y variables
 st.scatter_chart(data = data, x = x, y = y)
 
